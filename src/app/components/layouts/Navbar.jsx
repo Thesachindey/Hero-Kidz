7fa3781c7ff3517fdb0bd41acaac736cf902cf14
Sachin <sachinscode24@gmail.com>
2026-02-20 17:21:59 +0600

    added

--- src/app/components/layouts/Navbar.jsx
+++ src/app/components/layouts/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Logo from "./Logo";
+import NavLinks from "../buttons/NavLinks";
 
 const Navbar = () => {
+  const menuItems = <>
+  <li className="flex flex-row items-center justify-center gap-2">
+  <NavLinks href={'/'}>Home</NavLinks>
+  <NavLinks href={'/about'}>About</NavLinks>
+  <NavLinks href={'/contact'}>Contact</NavLinks>
+  </li>
+  </>
   return (
     <div>
       <div className="navbar bg-base-100">
@@ -26,48 +34,14 @@ const Navbar = () => {
             <ul
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menuItems}
             </ul>
           </div>
          <Logo />
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menuItems}
           </ul>
         </div>
         <div className="navbar-end">
